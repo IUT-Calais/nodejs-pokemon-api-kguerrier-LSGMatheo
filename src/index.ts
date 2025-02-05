7ebfd75dce1969f0c5677dfbf32fc5c3063ba221
LSGMatheo <math.lesage74@gmail.com>
2025-02-05 08:48:59 +0100

    Ajout des routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Request, Response } from 'express';
+
 
 export const app = express();
 const port = process.env.PORT || 3000;
@@ -10,3 +12,23 @@ export const server = app.listen(port);
 export function stopServer() {
   server.close();
 }
+
+app.get('/pokemons-cards', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
+
+app.get('/pokemons-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons avec id');
+});
+
+app.post('/pokemons-cards', (_req: Request, _res: Response) => {
+  _res.status(200).send('post: Liste de tous les Pokémons');
+});
+
+app.patch('/pokemon-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('post: Liste de tous les Pokémons avec id');
+});
+
+app.delete('/pokemon-cards/:pokemonCardId', (_req: Request, _res: Response) => {
+  _res.status(200).send('Liste de tous les Pokémons');
+});
